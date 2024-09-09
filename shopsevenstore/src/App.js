cf883251e9cbbdb6e8ed39ea2bdfdb3b4526a5d1
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-09-09 15:08:36 -0300

    estruturando projeto e dando inicio aos itens da navbar

--- shopsevenstore/src/App.js
+++ shopsevenstore/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (  
+    <Router>
+      <div className="App">
+      <div className="nav">
+        <h1 className="logo">
+          SHOP7<span>STORE</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Produtos</Link>
+            </li>
+            <li>
+              <Link to="/about">Quem Somos</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contato</Link>
+            </li>
+            <li>
+              <Link to="/account">Meu Carrinho</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
+    </Router>
   );
 }
 
