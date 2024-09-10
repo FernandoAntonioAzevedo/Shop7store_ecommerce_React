4865a6edaae6c359c7ca9f810304332276779fda
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-09-10 18:11:26 -0300

    consumindo db.json com os products do projeto

--- shopsevenstore/src/App.js
+++ shopsevenstore/src/App.js
@@ -2,8 +2,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import ProductsList from "./components/ProductsList";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/db.json')
+    .then((res) => res.json())
+    .then((data) => setProducts(data.products));
+  }, [])
+
   return (  
     <Router>
       <div className="App">
@@ -12,7 +22,13 @@ function App() {
           <Header />
           <div className="page-inner-content">
             <div className="section-title">
-            </div>            
+              <h3>Produtos Selecionados</h3>
+              <div className="underline"></div>
+            </div> 
+
+            <div className="main-content">
+              <ProductsList products={products} />
+            </div>           
           </div>
         </main>
       </div>
