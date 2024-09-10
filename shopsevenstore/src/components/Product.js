f27155095b2e50b8a63f9a91bf80ffe21037e235
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-09-10 20:00:47 -0300

    estilizando a section products e seus conteúdos

--- shopsevenstore/src/components/Product.js
+++ shopsevenstore/src/components/Product.js
@@ -1,4 +1,7 @@
+import { faCartShopping, faMoneyBill } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Product
 ({id, image, name, rate, price}) {
@@ -8,7 +11,19 @@ export default function Product
         <img src={image} alt={name} />
         <p className="name">{name}</p>
         <p className="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
-        <p className="price"><span>R$</span> {price}</p>
+        <p className="price">
+            <span>R$</span> {price}
+        </p>
+        <div className="buttons">
+            <Link to="/products/123/checkout" className="btn-icon">
+                <span>Comprar Agora</span>
+                <FontAwesomeIcon icon={faMoneyBill} />
+            </Link>
+            <button className="btn-icon add-to-cart-btn">
+                <span>Adicionar ao Carrinho</span>
+                <FontAwesomeIcon icon={faCartShopping} />
+            </button>
+        </div>
     </div>    
   );
 }
