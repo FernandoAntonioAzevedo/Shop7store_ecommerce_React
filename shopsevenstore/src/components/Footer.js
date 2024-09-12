08f45a2f75b036ecf4a357342e49487f713e8f37
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2024-09-12 15:39:42 -0300

    finalizando estilizações do footer e dando inicio as funcionalidades do carrinho

--- shopsevenstore/src/components/Footer.js
+++ shopsevenstore/src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <footer>
-      <div className="pager-inner-content content">
+      <div className="page-inner-content content">
           <div className="download-options">
              <p>Baixo nosso Aplicativo Mobile</p>
              <p>Disponível para Android e IOS</p>
@@ -13,7 +13,7 @@ export default function Footer() {
                <img src="/images/play-store.png" alt="Play Store download" />
              </div>
           </div>
-          <div className="logo.footer">
+          <div className="logo-footer">
             <h1 className="logo">
               SHOP7<span>STORE</span>  
             </h1>
@@ -26,16 +26,27 @@ export default function Footer() {
             <ul>
               <li>
                 <Link to="/">Cupons</Link>
+              </li>
+              <li>  
                 <Link to="/">Blog Post</Link>
+              </li>
+              <li>  
                 <Link to="/">políticas de Compra</Link>
+              </li>
+              <li>  
                 <Link to="/">Torne-se Afiliado</Link>
               </li>  
             </ul>
-          </div>
-
-          <hr/>
-
-      </div>
+          </div>            
+        </div>
+        <div className="page-inner-content">
+        <hr />
+            <p className="copyright">
+                Copyright 2024 - 
+                <a href="https://beratech.com.br/" target="_blank"> Beratechsolutions </a>
+                 - All Rights Reserved
+            </p>
+        </div>  
   </footer>
   );      
 }
